6318143549a4aa8308d89553163b459f5a623403
Oktavian Aji <oktavian.azis@sigma.co.id>
2025-04-11 16:29:55 +0700

    remove unused files

--- model/loveCount.model.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const loveCountSchema = new mongoose.Schema({
-  count: { type: Number, default: 0 },
-});
-
-export default mongoose.models.LoveCount ||
-  mongoose.model("LoveCount", loveCountSchema);
